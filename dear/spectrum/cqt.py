f42054484260cda08d73f9e6ff456a4c59aee213
Dongying Zhang <zhdongying@gmail.com>
2012-02-01 18:44:34 +0800

    DFT

--- dear/spectrum/cqt.py
+++ dear/spectrum/cqt.py
@@ -1,3 +1,20 @@
 #-*- coding: utf-8 -*-
 
+from _base import SpectrumBase
+import numpy
 
+
+class Spectrum(SpectrumBase):
+    '''Spectrum of Constant-Q Transform'''
+
+    @staticmethod
+    def pre_calculate(win, win_shape):
+        pass
+
+    @staticmethod
+    def transform(samples, win_shape=numpy.hamming, pre_var=None):
+        pass
+
+    def walk(self, Q, win, step, start=0, end=None, join_channels=True,
+            win_shape=numpy.hamming):
+        var = self.pre_caculate(win, win_shape)
--- dear/spectrum/dft.py
+++ dear/spectrum/dft.py
@@ -7,18 +7,15 @@ import numpy
 class Spectrum(SpectrumBase):
     '''Spectrum of Discrete Fourier Transform'''
     
-    def _pre_calculate(self, win, step, win_shape):
+    @staticmethod
+    def pre_calculate(win, win_shape):
         var = {
             'WL': win,
-            'SL': step,
-            'SR': self.audio.samplerate,
-            'D': self.audio.duration,
-            'CHN': self.audio.channels,
             'W': win_shape(win)}
         #
         PRE = []
         arr = 2. * numpy.pi * numpy.arange(win) / win
-        for k in xrange(win/2):
+        for k in xrange(win/2+1):
             PRE.append(
                 var['W'] * (numpy.cos(arr*k) - numpy.sin(arr*k)*1j) 
             )
@@ -26,24 +23,27 @@ class Spectrum(SpectrumBase):
         #
         return type('variables', (object,), var)
 
-    def walk(self, win=1024, step=512, start=0, end=None, join_channels=True, 
+    @staticmethod
+    def transform(samples, win_shape=numpy.hamming, pre_var=None):
+        if not pre_var:
+            pre_var = Spectrum.pre_calculate(len(samples), win_shape)
+        frame = numpy.array(
+                [numpy.sum(samples * pre) for pre in pre_var.PRE])
+        return frame / pre_var.WL
+
+    def walk(self, win=1024, step=512, start=0, end=None, join_channels=True,
             win_shape=numpy.hamming):
-        ''''''
-        var = self._pre_calculate(win, step, win_shape)
+        var = self.pre_calculate(win, win_shape)
         #
         if join_channels:
             for samples in self.audio.walk(win, step, start, end, 
                     join_channels):
-                frame = numpy.array(
-                        [numpy.sum(samples * pre) for pre in var.PRE])
-                yield frame / var.WL
+                yield self.transform(samples, pre_var=var)
         else:
             for samples in self.audio.walk(win, step, start, end, 
                     join_channels):
                 frame = []
                 for ch in samples:
-                    data = numpy.array(
-                            [numpy.sum(ch * pre) for pre in var.PRE])
-                    frame.append(data / var.WL)
+                    frame.append(self.transform(ch, pre_var=var))
                 yield frame
 
