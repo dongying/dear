ce6e39ea913efe68e2266797fbac8a8c271d84be
Dongying Zhang <zhdongying@gmail.com>
2012-04-27 12:24:06 +0800

    license and setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dear',
+    version='0.1',
+    description='Dear EARs Audio Analysis Framework',
+    author='Dongying Zhang',
+    author_email='zhdongying@gmail.com',
+    url='http://dongying.github.com/dear',
+    packages=['dear','dear.spectrum','dear.analysis', 'dear.io'],
+    license='MIT LICENSE',
+)
+
